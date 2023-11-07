75cf63f1b3ff3e2d2698c086a927aba84148f9fe
Akash.Reddy <29536523+akashreddy-gg@users.noreply.github.com>
2023-11-07 20:26:15 +0530

    add spring web mvc & RestController

--- src/main/java/com/akashreddy/myfancypdfinvoices/context/ApplicationConfiguration.java
+++ src/main/java/com/akashreddy/myfancypdfinvoices/context/ApplicationConfiguration.java
@@ -6,13 +6,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
 //@ComponentScan(basePackages = "com.akashreddy.myfancypdfinvoices")
 @PropertySource("classpath:/application.properties")
 @PropertySource(value = "classpath:/application-${spring.profiles.active}.properties", ignoreResourceNotFound = true)
-public class MyFancyPdfInvoicesApplicationConfiguration {
+@EnableWebMvc
+public class ApplicationConfiguration {
+
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        return new MethodValidationPostProcessor();
+    }
 
     @Bean
     public ObjectMapper objectMapper() {
